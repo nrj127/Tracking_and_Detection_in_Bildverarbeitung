1512385defa263908afe84c8ed371b5606fd7eb6
ga37qez <n.p.sujan@tum.de>
2015-10-25 12:52:42 +0100

    added 1d gaussian function

--- Exercise/Ex2.m
+++ Exercise/Ex2.m
@@ -20,3 +20,16 @@ title('Original Image')
 figure(4)
 imshow(uint8(convoluted_image))
 title('convoluted image with 2D Gaussian Kernel')
+
+kernelyDirection = GaussianSeparate(sigma,'yDirection');
+kernelxDirection = GaussianSeparate(sigma,'xDirection');
+
+tic
+convoluted_image_temp = convolution(padded_image,kernelxDirection);
+convoluted_image_temp= do_padding(convoluted_image_temp,kernelyDirection,'border');
+convoluted_image_final = convolution(convoluted_image_temp, kernelyDirection);
+toc
+
+figure(16)
+imshow(uint8(convoluted_image_final))
+title('convoluted image with 1D Gaussian')
