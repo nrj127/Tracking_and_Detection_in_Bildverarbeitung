f7c1f75111712cedd126a62559e2fcc137e0c4b2
Ansh Kapil <anshkapil@gmail.com>
2015-11-02 00:28:58 +0100

    Bilateral Filtering done

--- Exercise/Assignment2/Ex2.m
+++ Exercise/Assignment2/Ex2.m
@@ -1,3 +1,16 @@
-I = imread('lena.gif');
+I = imread('bb.jpg');
+I = rgb2gray(I);
+sigma_d =15; sigma_r = 15;
+bilateralFiltered = bilateralFilter(I,sigma_d,sigma_r,'mirror');
 
-result1 = bilateralFilter(I,[3 3],1,1);
+figure(1)
+subplot(1,2,1)
+imshow(uint8(I)) % Original
+title('Original')
+
+hold on
+subplot(1,2,2)
+imshow(uint8(bilateralFiltered)) % Median Filtered
+title(strcat('Filter = ',num2str(3*sigma_d),'X',num2str(3*sigma_d)))
+hold off
+
