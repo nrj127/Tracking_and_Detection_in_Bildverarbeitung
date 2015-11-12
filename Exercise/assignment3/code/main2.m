95eb59e24be41dceda86d5b9febd406cda4248cb
ga37qez <n.p.sujan@tum.de>
2015-11-12 08:12:57 +0100

    clean up

--- Exercise/assignment3/code/main2.m
+++ Exercise/assignment3/code/main2.m
@@ -13,26 +13,25 @@ s0 = 0.7;
 k = 1.4;
 alpha = 0.04;
 
-%n = [0 7 17];
+
 
 n = 7;
 
-   % th =  10000000; %10000
-   % tl = 50  ;   %10
-    th = 1000;
-    tl = 10;
-   f = Harris_Laplace(im,s0,k,alpha,th,tl,n)
+% th =  10000000; %10000
+% tl = 50  ;   %10
+th = 1000;
+tl = 10;
+f = Harris_Laplace(im,s0,k,alpha,th,tl,n)
 
-   clc;close all;
-   figure,imshow(im);
-   hold on;
-   
-   for i = 1:size(f,1)
-       circle_size = s0 * k^(f(i,3)) * 6;
-       plot(f(i, 2), f(i, 1), 'o', 'MarkerSize', circle_size);
-       
-   end
+clc;close all;
+figure,imshow(im);
+hold on;
+
+for i = 1:size(f,1)
+    circle_size = s0 * k^(f(i,3)) * 6;
+    plot(f(i, 2), f(i, 1), 'o', 'MarkerSize', circle_size);
+    
+end
 
 
 
- 
