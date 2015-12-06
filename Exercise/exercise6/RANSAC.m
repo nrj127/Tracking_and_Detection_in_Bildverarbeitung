2be5b8e30ec5a9ff1ded5ab8ba22fa37d20be3b7
ga37qez <n.p.sujan@tum.de>
2015-12-06 18:06:50 +0100

    added DLT,RANSAC,NORMALIZE

---
+++ Exercise/exercise6/RANSAC.m
@@ -0,0 +1,29 @@
+function [ H] = RANSAC(X,Y,p,td,tset)
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+
+N = inf;
+sample_count = 1;
+done = 0;
+best = 0;
+bestinliers = 0;
+
+while N > sample_count && ~done
+    rndset = randi([1 size(X,2)],1,4);
+    H = DLT(X(:,rndset),Y(:,rndset));
+    X1_dash = H*X;
+    X1_dash = X1_dash./repmat(X1_dash(3,:),3,1);
+    inliers=sqrt(sum((X1_dash(1:2,:)-Y(1:2,:)).^2))<td;
+    if sum(inliers)>best
+    	bestinliers=inliers;
+    end
+    if sum(inliers)>=tset
+        done=1;
+    end
+    epsilon=1-sum(inliers)/numel(inliers);
+    N=round(log(1-p)/log(1-(1-epsilon)^4));
+    sample_count=sample_count+1;
+end
+H = DLT(X(:,bestinliers),Y(:,bestinliers))
+end
+
---
+++ Exercise/exercise6/normalize.m
@@ -0,0 +1,36 @@
+function [newpts,T] = normalize(pts)
+%UNTITLED3 Summary of this function goes here
+%   Detailed explanation goes here
+
+
+
+if(size(pts,1)  ~= 3)
+    error('pts must be 3xN');
+end
+
+finiteind = find(abs(pts(3,:)) > eps);
+
+if length(finiteind) ~= size(pts,2)
+    warning('Some points are at infinity');
+end
+
+pts(1,finiteind) = pts(1,finiteind)./pts(3,finiteind);
+pts(2,finiteind) = pts(2,finiteind)./pts(3,finiteind)
+pts(3,finiteind) = 1;
+
+c = mean(pts(1:2, finiteind)')';
+newp(1,finiteind) = pts(1,finiteind) - c(1);
+newp(2,finiteind) = pts(2,finiteind) - c(2);
+
+dist = sqrt(newp(1,finiteind).^2 + newp(2,finiteind).^2);
+meandist = mean(dist(:));
+
+scale = sqrt(2)/meandist;
+
+T = [scale 0 -scale*c(1)
+     0     scale   -scale*c(2)
+     0    0         1];
+ 
+ newpts = T*pts;
+
+pts
