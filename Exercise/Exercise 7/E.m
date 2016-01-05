7b8cf3711ee31431e4a6655c69130a353c0d2458
Ansh Kapil <anshkapil@gmail.com>
2016-01-05 13:43:33 +0100

    Ex 7 final..

--- Exercise/Exercise 7/E.m
+++ Exercise/Exercise 7/E.m
@@ -1,17 +1,28 @@
-function energy = E( params , A, M, m )
+function energy = E( params , A, M, m , method )
 %E Energy Function
+if(strcmp(method , 'euler') == 1)
+    alpha = params(1);
+    beta = params(2);
+    gamma = params(3);
 
+    RX = [ 1, 0, 0; 0, cos(alpha), sin(alpha); 0, -sin(alpha), cos(alpha)];
+    RY = [cos(beta), 0, -sin(beta); 0,1,0; sin(beta),0,cos(beta)];
+    RZ = [ cos(gamma), sin(gamma) , 0; -sin(gamma), cos(gamma) , 0 ; 0 , 0 , 1];
 
-alpha = params(1);
-beta = params(2);
-gamma = params(3);
+    R = RZ * RY * RX;
+    t = params(4:6);
+end
 
-RX = [ 1, 0, 0; 0, cos(alpha), sin(alpha); 0, -sin(alpha), cos(alpha)];
-RY = [cos(beta), 0, -sin(beta); 0,1,0; sin(beta),0,cos(beta)];
-RZ = [ cos(gamma), sin(gamma) , 0; -sin(gamma), cos(gamma) , 0 ; 0 , 0 , 1];
+if(strcmp(method , 'quaternions') == 1)
+        x = params(1);
+        y = params(2);
+        z = params(3);
+        w = params(4);
 
-R = RZ * RY * RX;
-t = params(4:6);
+        R = RotationMatrix( [w;x;y;z], 'quaternion'); 
+              R = R.matrix;
+        t = params(5:7);     
+end
 
 M = [ M; ones(1, size(M,2))];
 proj_mat = A*[R , t];
