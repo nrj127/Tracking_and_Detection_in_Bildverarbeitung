21afc94b08f580854a4485059931da7f1837e8c7
Ansh Kapil <anshkapil@gmail.com>
2016-01-20 13:31:57 +0100

    Histogram creation updated

--- Exercise/ex10/colorHist.m
+++ Exercise/ex10/colorHist.m
@@ -4,7 +4,17 @@ function [ h ] = colorHist( region )
     hsv = rgb2hsv(region);
     hue = hsv(:,:,1);
     
-    h = hist(hue(:), 256);
-    hist(hue(:), 256);
+    range = 0: (1/256) : 1;
+    
+    for i=1:256
+       
+        h(i) = sum(sum( hue>=range(i) & hue<range(i+1)));
+        
+    end
+    bar(h)
+    
+    %% For debugging
+    %h = hist(hue(:), 256);
+    %hist(hue(:), 256);
 
 end
